db73be58c751a1e192434666b68924680e13f861
wy9933 <583049572@qq.com>
2023-09-12 00:09:44 +0800

    add color setting & refine menu

--- TaskbarWidget.py
+++ TaskbarWidget.py
@@ -15,10 +15,12 @@ from PyQt5.QtWidgets import *
 
 class TaskbarWidget(QWidget):
     def __init__(self) -> None:
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
         """初始化TaskbarWidget
         """
-        super().__init__()
-
         # 获取win10本身taskbar的各级window
         m_hTaskbar = win32gui.FindWindow("Shell_TrayWnd", None)
         m_hBar = win32gui.FindWindowEx(m_hTaskbar, 0, "ReBarWindow32", None)
@@ -35,23 +37,46 @@ class TaskbarWidget(QWidget):
 
         # 右键菜单
         self.rightMenu = QMenu(self)
-        self.actionA = QAction("退出", self)
+        self.actionSettings = QAction("设置", self, triggered=self.menuSettings)
+        self.actionQuit = QAction("退出", self, triggered=self.menuExit)
+
+        self.rightMenu.addAction(self.actionSettings)
+        self.rightMenu.addAction(self.actionQuit)
+
         # *右键点击弹出菜单的设置和绑定
         self.setContextMenuPolicy(Qt.CustomContextMenu)
         self.customContextMenuRequested.connect(self.init_rightMenu)
 
+        # 设置颜色 R G B alpha
+        palette = self.palette()
+        palette.setColor(QPalette.Background, QColor(0, 0, 0, 50))
+        self.setPalette(palette)
+        
     def exitapp(self):
         """退出
         """
         app = QApplication.instance()
         app.quit()
 
+    def menuSettings(self):
+        """menu中点击设置调用逻辑
+        """
+        # TODO: 设置的相关逻辑
+        pass
+
+    def menuExit(self):
+        """menu中点击退出调用逻辑
+        """
+        self.exitapp()
+
     def init_rightMenu(self):
         """初始化右键菜单
         """
-        self.rightMenu.addAction(self.actionA)
-        self.actionA.triggered.connect(self.exitapp)
         self.rightMenu.popup(QCursor.pos())
+
+    # 保留退出手段，用于测试时没有其他退出手段时使用
+    def mouseDoubleClickEvent(self, e: QMouseEvent) -> None:
+        self.exitapp()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
